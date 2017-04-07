2f32c344b03a336b87f9d03cd95311af0671ddc9
Obsidian <ObsidianRock@users.noreply.github.com>
2017-04-07 18:38:07 +0100

    leveraging the authentication framework

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,15 @@
 
 from django.conf.urls import url
+from django.contrib.auth.views import login, logout, logout_then_login
 
 from . import views
 
 
 urlpatterns = [
 
-    url(r'^login/$', views.user_login, name='login'),
+    #url(r'^login/$', views.user_login, name='login'), raw example
+
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^logout-then-login/$', logout_then_login, name='logout_then_login')
 ]
