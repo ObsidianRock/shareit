54d42c47edabd32e6e184fc2ff961bf06b1da7a3
Obsidian <ObsidianRock@users.noreply.github.com>
2017-04-08 12:29:57 +0100

    added Reset password views and templates

--- account/urls.py
+++ account/urls.py
@@ -21,4 +21,10 @@ urlpatterns = [
     url(r'^password-change/$', password_change, name='password_change'),
     url(r'^password-change/done/$', password_change_done, name='password_change_done'),
 
+    url(r'^password-reset/$', password_reset, name='password_reset'),
+    url(r'^password-reset/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+        password_reset_confirm,
+        name='password_reset_confirm'),
+    url(r'^password-reset/complete/$', password_reset_complete, name='password_reset_complete'),
 ]
