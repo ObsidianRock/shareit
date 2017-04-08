39ac588b77ae53ccb8baf2df9a9b866f4c1b8df5
Obsidian <ObsidianRock@users.noreply.github.com>
2017-04-08 15:34:49 +0100

    added google OAuth2 for authentication

--- shareit/urls.py
+++ shareit/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'account/', include('account.urls'))
+    url(r'account/', include('account.urls')),
+    url('social-auth/', include('social.apps.django_app.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
