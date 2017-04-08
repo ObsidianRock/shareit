a23f077ecf80ea3adb690b584f84194afe0b9662
Obsidian <ObsidianRock@users.noreply.github.com>
2017-04-08 14:04:53 +0100

    added edit profile and and templates

--- account/urls.py
+++ account/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     url(r'^password-reset/complete/$', password_reset_complete, name='password_reset_complete'),
 
     url(r'^register/$', views.register, name='register'),
+    url(r'^edit/$', views.edit, name='edit'),
 ]
--- account/views.py
+++ account/views.py
@@ -7,7 +7,8 @@ from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth import authenticate, login
 
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 
 
@@ -57,6 +58,7 @@ def register(request):
             new_user.set_password(
                 user_form.cleaned_data['password'])
             new_user.save()
+            profile = Profile.objects.create(user=new_user)
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -67,3 +69,27 @@ def register(request):
     return render(request,
                   'account/register.html',
                   {'user_form': user_form})
+
+@login_required
+def edit(request):
+
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                                       data=request.POST,
+                                       files=request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
+
