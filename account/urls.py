9511a79bb229c5820dea8b4037e3517c1fc753c3
Obsidian <ObsidianRock@users.noreply.github.com>
2017-04-08 12:15:29 +0100

    added change password views and template

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,9 @@
 
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout, logout_then_login
+from django.contrib.auth.views import (login, logout, logout_then_login,
+                                       password_change, password_change_done,
+                                       password_reset, password_reset_done,
+                                       password_reset_confirm, password_reset_complete)
 
 from . import views
 
@@ -15,5 +18,7 @@ urlpatterns = [
 
 
     url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^password-change/$', password_change, name='password_change'),
+    url(r'^password-change/done/$', password_change_done, name='password_change_done'),
 
 ]
